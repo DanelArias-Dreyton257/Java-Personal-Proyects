5aa5d2980219550616e2dd45a81143509478ba1f
danel <danel@DESKTOP-3DCJVBQ>
2021-01-30 17:09:40 +0100

    extra changes in window...is supposed to work

--- Time Optimizer/src/visuals/TaskWindow.java
+++ Time Optimizer/src/visuals/TaskWindow.java
@@ -25,14 +25,14 @@ import objects.TaskOrganizer;
 public class TaskWindow extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	private static final int WIDTH = 600;
+	private static final int WIDTH = 700;
 	private static final int HEIGHT = 500;
-	
+
 	private TaskOrganizer tOrg = new TaskOrganizer();
 
 	private JList<Task> lsTasks = new JList<Task>();
 	private DefaultListModel<Task> mdLsTasks = new DefaultListModel<>();
-	
+
 	private JLabel lbName = new JLabel("NULL");
 	private JLabel lbDeadline = new JLabel("XX/XX/XXXX");
 
@@ -42,17 +42,21 @@ public class TaskWindow extends JFrame {
 	private JPanel pnTaskManager = new JPanel();
 	private JPanel pnCentral = new JPanel();
 	private JPanel pnTaskCentral = new JPanel();
-	
+
 	private JTextField txName = new JTextField(20);
+	private JTextField txNumHours = new JTextField(4);
+	private JTextField txDay = new JTextField(3);
+	private JTextField txMonth = new JTextField(3);
+	private JTextField txYear = new JTextField(6);
 	private JSlider slImportance = new JSlider(JSlider.HORIZONTAL, 1, 10, 5);
 
 	private JButton btAdd = new JButton("Add Task");
 
 	public TaskWindow() {
-		Task t1 = new Task("Programar Tareas", new GregorianCalendar(2021, 2-1, 7),27, 6);
-		Task t2 = new Task("Ordenar y limpiar habitacion", new GregorianCalendar(2021, 2-1, 7),22, 7);
-		Task t4 = new Task("Leds Vitrina", new GregorianCalendar(2021, 3-1, 31),2, 6);
-		tOrg.addAll(t1, t2, t4);
+//		Task t1 = new Task("Programar Tareas", new GregorianCalendar(2021, 2 - 1, 7), 27, 6);
+//		Task t2 = new Task("Ordenar y limpiar habitacion", new GregorianCalendar(2021, 2 - 1, 7), 22, 7);
+//		Task t4 = new Task("Leds Vitrina", new GregorianCalendar(2021, 3 - 1, 31), 2, 6);
+//		tOrg.addAll(t1, t2, t4);
 //		for (Task t: tOrg) {
 //			System.out.println(t);
 //			System.out.println(t.getDeadlineInString());
@@ -95,8 +99,14 @@ public class TaskWindow extends JFrame {
 			}
 		});
 		pnTaskManager.setLayout(new BorderLayout());
-		pnTaskManager.add(pnTaskCentral,BorderLayout.CENTER);
+		pnTaskManager.add(pnTaskCentral, BorderLayout.CENTER);
 		pnTaskCentral.add(txName);
+		pnTaskCentral.add(txNumHours);
+		pnTaskCentral.add(txDay);
+		pnTaskCentral.add(new JLabel("/"));
+		pnTaskCentral.add(txMonth);
+		pnTaskCentral.add(new JLabel("/"));
+		pnTaskCentral.add(txYear);
 		pnTaskCentral.add(slImportance);
 		slImportance.setMajorTickSpacing(1);
 		slImportance.setPaintTicks(true);
@@ -108,7 +118,35 @@ public class TaskWindow extends JFrame {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+				Task t = null;
+				String name = txName.getText();
+				String hoursStr = txNumHours.getText();
+				String dayStr = txDay.getText();
+				String monthStr = txMonth.getText();
+				String yearStr = txYear.getText();
+				int importance = slImportance.getValue();
+				checkAndAddTask(name, hoursStr, dayStr, monthStr, yearStr, importance);
+				updateList();
+
+			}
+
+			private void checkAndAddTask(String name, String hoursStr, String dayStr, String monthStr, String yearStr,
+					int importance) {
+				int numHours = 0;
+				int day = 0;
+				int month = 0;
+				int year = 0;
+				try {
+					numHours = Integer.parseInt(hoursStr);
+					day = Integer.parseInt(dayStr);
+					month = Integer.parseInt(monthStr);
+					year = Integer.parseInt(yearStr);
+				} catch (NumberFormatException e) {
+					throw new IllegalArgumentException("The strings cant be parsed", e);
+				}
+				if (numHours>0 && day>0 && day<=31 && year>=2020 && month>0 && month<=12) {
+					tOrg.add(new Task(name, new GregorianCalendar(year, month-1, day), numHours, importance));
+				}
 
 			}
 		});
